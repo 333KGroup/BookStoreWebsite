2c6d671b735a594db2ac373272322fa6e5031ef8
Jonathan Malott <contact@geronimobile.com>
2015-11-11 21:13:43 -0600

    added book model, needs more properties

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models {
+    public class Book {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int UniqueNumber { get; set; }
+
+    }
+}
